4248fe94f2bbec82d8fbde68c7ea46103a0e5099
Yiting Chang <yiting.chang@iths.se>
2025-03-03 19:20:51 +0100

    Update NationalCodeService.java

--- src/main/java/se/yitingchang/nationalcodesearch/NationalCodeService.java
+++ src/main/java/se/yitingchang/nationalcodesearch/NationalCodeService.java
@@ -1,4 +1,33 @@
 package se.yitingchang.nationalcodesearch;
 
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
 public class NationalCodeService {
+    private final Map<String,String> countryCodes=new HashMap<>();
+
+    public NationalCodeService() {
+        countryCodes.put("US","United States");
+        countryCodes.put("CA","Canada");
+        countryCodes.put("GB","United Kingdom");
+        countryCodes.put("AU","Australia");
+        countryCodes.put("DE","Germany");
+        countryCodes.put("IN","India");
+        countryCodes.put("JP","Japan");
+        countryCodes.put("CN","China");
+        countryCodes.put("KR","Korea");
+        countryCodes.put("PL","Poland");
+        countryCodes.put("RU","Russia");
+        countryCodes.put("SE","Sweden");
+
+    }
+
+    public String getCountryCode(String country) {
+        return countryCodes.entrySet().stream().filter(entry -> entry.getValue().equalsIgnoreCase(country)).map(Map.Entry::getKey).findFirst().orElse("Unknown code");
+    }
+
+
 }
